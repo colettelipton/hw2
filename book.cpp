1c234a7f34b0ce22940924cafa84805c43a722b3
Colette Lipton <clipton@usc.edu>
2023-02-13 19:29:28 +0000

    add back deleted lines

--- book.cpp
+++ book.cpp
@@ -8,14 +8,17 @@ Book::Book(const std::string category, const std::string name, double price,
     author_ = author;
 }
 
-std::set<std::string> Book::keywords() const {SBN_);
-    // QUESTION: is the type a keyword?> set2 = parseStringToWords(convToLower(author_));
+std::set<std::string> Book::keywords() const {
+    // QUESTION: is the type a keyword?> 
+		set<string> set1;
+		set1.insert(ISBN_);
+		set<string> set2 = parseStringToWords(convToLower(author_));
     set<string> set3 = parseStringToWords(convToLower(name_));
-    
+		
     // ensures only words with 2+ letters are added
     for(set<string>::iterator it = set2.begin(); it != set2.end(); it++){
-		if((*it).size() >= 2) set1.insert(*it);
-	}
+			if((*it).size() >= 2) set1.insert(*it);
+		}
     
     // ensures only words with 2+ letters are added
     for(set<string>::iterator it = set3.begin(); it != set3.end(); it++){
